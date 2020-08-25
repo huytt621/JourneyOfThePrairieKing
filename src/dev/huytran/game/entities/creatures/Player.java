715a7e10c23039740021fdc9f03ab2106e8921b8
Huy Tran <huytt621@gmail.com>
2020-08-25 13:49:50 -0700

    Assets can be drawn at different sizes and revamped key movement

--- src/dev/huytran/game/entities/creatures/Player.java
+++ src/dev/huytran/game/entities/creatures/Player.java
@@ -11,28 +11,36 @@ public class Player extends Creature {
     private float speed = 3;
 
     public Player(Game game, float x, float y) {
-        super(x, y);
+        super(x, y, DEFAULT_CREATURE_WIDTH, DEFAULT_CREATURE_HEIGHT);
         this.game = game;
+    }
+
+    private void getInput() {
+        xMove = 0;
+        yMove = 0;
+
+        if (game.getKeyManager().up) {
+            yMove = -speed;
+        }
+        if (game.getKeyManager().down) {
+            yMove = speed;
+        }
+        if (game.getKeyManager().left) {
+            xMove = -speed;
+        }
+        if (game.getKeyManager().right) {
+            xMove = speed;
+        }
     }
 
     @Override
     public void tick() {
-        if (game.getKeyManager().up) {
-            y -= speed;
-        }
-        if (game.getKeyManager().down) {
-            y += speed;
-        }
-        if (game.getKeyManager().left) {
-            x -= speed;
-        }
-        if (game.getKeyManager().right) {
-            x += speed;
-        }
+        getInput();
+        move();
     }
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.player, (int) x, (int) y, null);
+        g.drawImage(Assets.player, (int) x, (int) y, width, height, null);
     }
 }
--- src/dev/huytran/game/gfx/Assets.java
+++ src/dev/huytran/game/gfx/Assets.java
@@ -4,16 +4,15 @@ import java.awt.image.BufferedImage;
 
 public class Assets {
 
-    private static final int SPRITE_WIDTH = 64;
-    private static final int SPRITE_HEIGHT = 64;
+    private static final int SPRITE_WIDTH = 16;
+    private static final int SPRITE_HEIGHT = 16;
 
     public static BufferedImage player;
     public static BufferedImage title;
 
     public static void init() {
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/sheet.png"));
-        player = sheet.crop(SPRITE_WIDTH * 23 - 4, SPRITE_HEIGHT * 7, SPRITE_WIDTH, SPRITE_HEIGHT);
-        title = sheet.crop(0, 320, 380, 388);
+        player = sheet.crop(367, 112, SPRITE_WIDTH, SPRITE_HEIGHT);
     }
 
 }
